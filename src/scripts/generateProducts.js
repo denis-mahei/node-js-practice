347e3f3ae7596175fbc2fe388a6ed08094ae7f2f
denis-mahei <denmahei@gmail.com>
2025-07-09 17:40:21 +0300

    rm node_modules

--- src/scripts/generateProducts.js
+++ src/scripts/generateProducts.js
@@ -1,3 +1,10 @@
-import { createFakeProduct } from '../utils/createFakeProducts.js';
+import fs from 'node:fs/promises';
 
-const generateProducts = () => {};
+import { createFakeProduct } from '../utils/createFakeProducts.js';
+import { PATH_DB } from '../constants/products.js';
+
+const generateProducts = async (amount) => {
+  try {
+    const data = await fs.readFile(PATH_DB);
+  } catch (e) {}
+};
