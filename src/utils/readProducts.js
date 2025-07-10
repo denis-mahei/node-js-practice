dfb4878578572e2a3c38187f2f3e441cdd0096e9
denis-mahei <denmahei@gmail.com>
2025-07-10 22:03:48 +0300

    init repo products

--- src/utils/readProducts.js
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs/promises';
-import { PATH_DB } from '../constants/products.js';
-
-export const readProducts = async () => {
-  const data = await fs.readFile(PATH_DB);
-  const products = JSON.parse(data);
-  return await products;
-};
--- src/utils/writeProducts.js
+++
@@ -1,5 +0,0 @@
-import fs from 'node:fs/promises';
-import { PATH_DB } from '../constants/products.js';
-
-export const writeProducts = async (updatedProd) =>
-  await fs.writeFile(PATH_DB, JSON.stringify(updatedProd, null, 2));
