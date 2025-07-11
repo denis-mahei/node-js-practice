335b876e387ad6a421995af04aaef3369c919130
denis-mahei <denmahei@gmail.com>
2025-07-11 22:52:24 +0300

    add services & utils

--- src/services/products.js
+++ src/services/products.js
@@ -1,9 +1,36 @@
 import { Product } from '../models/products.js';
 
-export const getAllProducts = async () => {
-  return await Product.find();
+export const getAllProducts = async () => await Product.find();
+
+export const getProductById = async (id) => await Product.findById(id);
+
+export const createNewProduct = async (payload) => {
+  const product = await Product.create(payload);
+  return product;
 };
 
-export const getProductById = async (id) => {
-  return await Product.findById(id);
+export const deleteProduct = async (productId) => {
+  const product = await Product.findOneAndDelete({ _id: productId });
+  return product;
+};
+
+export const updateProduct = async (productId, payload, options = {}) => {
+  const rawResult = await Product.findOneAndUpdate(
+    {
+      _id: productId,
+    },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    product: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
 };
