2f9bfca010d987fda89901a2ff31d8cfa98741d4
denis-mahei <denmahei@gmail.com>
2025-07-13 10:52:08 +0300

    pagination done

--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -1,12 +1,13 @@
-export const calculatePaginationData = (count, page, perPage) => {
-  const totalPage = Math.ceil(count / perPage);
-  const hasNextPage = Boolean(totalPage - page);
+export const calculatePaginationData = (count, perPage, page) => {
+  const totalPages = Math.ceil(count / perPage);
+  const hasNextPage = Boolean(totalPages - page);
   const hasPrevPage = page !== 1;
 
   return {
     page,
     perPage,
     totalItems: count,
+    totalPages,
     hasNextPage,
     hasPrevPage,
   };
