7eec438ee89443ec66ca798068a40f6c773e4d22
denis-mahei <denmahei@gmail.com>
2025-07-14 12:43:20 +0300

    relocly /docs

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import path from 'path';
+import redoc from 'redoc-express';
+import { fileURLToPath } from 'url';
 import { sayHello } from './middlewares/welcome.js';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import productsRouter from './routers/products.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const PORT = getEnvVar('PORT', '3000');
 
@@ -27,6 +32,16 @@ export const setupServer = () => {
   exp.get('/', sayHello);
 
   exp.use('/api/products', productsRouter);
+
+  exp.use(
+    '/docs',
+    redoc({
+      title: 'Products API Docs',
+      specUrl: '/openapi.yaml',
+    }),
+  );
+
+  exp.use('/openapi.yaml', express.static(path.resolve(__dirname, 'docs')));
 
   exp.use(errorHandler);
 
